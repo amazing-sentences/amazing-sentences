37ab908c7c75fc9f90831a203a7fe29dd13f72bb
Jorge Juan Rodríguez Manzano <34060303+jjrmanzano@users.noreply.github.com>
2023-01-01 21:47:14 +0000

    Fixed mixed content http/https

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
         noExternal: ['@inertiajs/server'],
     },
     server: {
+        https: process.env.NODE_ENV === 'production',
         hmr: {
             host: 'localhost',
         },
