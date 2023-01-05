9cb611a64f9635e9a40ebe4e6710a6fba9d41550
Jorge Juan Rodríguez Manzano <34060303+jjrmanzano@users.noreply.github.com>
2023-01-05 15:41:48 +0100

    Install Laravel Jetstream and enable HTTPS for Vite development

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,35 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-export default defineConfig(({mode}) => ({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({mode}) => {
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                ssr: 'resources/js/ssr.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
-    ssr: {
-        noExternal: ['@inertiajs/server'],
-    },
-    server: {
-        https: mode === 'production',
-        hmr: {
-            host: 'localhost',
+            }),
+            mode === "development" ? basicSsl() : null
+        ],
+        ssr: {
+            noExternal: ['@inertiajs/server'],
         },
+        server: {
+            https: true,
+            hmr: {
+                host: 'localhost',
+                clientPort: 5174
+            },
+        }
     }
-}));
+});
